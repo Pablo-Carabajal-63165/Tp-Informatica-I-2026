c6b4452af0e55c694eb20b3b1a094c5156e732c3
Pablo-Carabajal-63165 <63165@electronica.frc.utn.edu.ar>
2026-06-20 23:32:32 -0300

    Subir código calculadora BIM

---
+++ bim.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	float p;
+	float a;
+	float bim;
+	
+	printf("Ingrese el peso en kg: ");
+	scanf("%f",&p);
+	printf("Ingrese su altura en metros: ");
+	scanf("%f",&a);
+	
+	bim=p/(a*a);
+	
+	printf("Su indice de masa corporal es: %.2f\n\n",bim);
+	
+	printf("Indice             | Condicion\n");
+	printf("------------------------------\n");
+	printf("Menor a 18.5       | Bajo Peso\n");
+	printf("18.5 a 24.9        | Normal\n");
+	printf("25.0 a 29.9        | Sobrepeso\n");
+	printf("Mayor o igual a 30 | Obesidad");
+	
+	
+	return 0;
+}
